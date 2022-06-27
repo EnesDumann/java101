850d9bc3be77edd47e5c0ef3f19755c868b7fed0
Enes DUMAN <duman0701@gmail.com>
2022-06-27 16:23:10 +0300

    java101

---
+++ src/Baslangic.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+public class Baslangic {
+    public static void main(String[] args) {
+        int mat,fizik,kimya,turkce,tarih,muzik;
+        Scanner inp = new Scanner(System.in);
+
+         System.out.print("Matematik Notunuz : ");
+         mat = inp.nextInt();
+
+         System.out.print("Fizik Notunuz : ");
+         fizik = inp.nextInt();
+
+         System.out.print("Kimya Notunuz : ");
+         kimya = inp.nextInt();
+
+         System.out.print("Turkce Notunuz : ");
+         turkce = inp.nextInt();
+
+         System.out.print("Tarih Notunuz : ");
+         tarih = inp.nextInt();
+
+         System.out.print("Muzik Notunuz : ");
+         muzik = inp.nextInt();
+
+         int toplam = (mat + fizik + kimya + turkce + tarih + muzik);
+         double sonuc = toplam / 6 ;
+         System.out.print(sonuc);
+         boolean kosul = sonuc >= 50 ;
+
+         String gectiMi  = (kosul ? " gectiniz": "kaldiniz:");
+         System.out.println(gectiMi);
+         }
+    }
